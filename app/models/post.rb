5d0da1ae77b36a7baa36f56d57ec2a7e07a253fc
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-06-28 18:56:40 -0700

    commit tree 8b1bec0920e5e6076b090bab251e495fcbf8b10e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :truefax
+
+    def truefax
+        if title == "True Facts"
+            errors.add(:title, "Not allowed!")
+        end
+    end
+
+
 end
